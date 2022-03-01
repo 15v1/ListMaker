deeaaf7ade6f9f980707fc203404ea1be116a48d
ALONE <68062657+haj4@users.noreply.github.com>
2022-03-01 20:29:12 +0200

    Create ListMkaerv2.cs

---
+++ ListMkaerv2.cs
@@ -0,0 +1,13 @@
+Random r=new Random();
+Console.Write("How much users: ");
+int s = int.Parse(Console.ReadLine());
+Console.Write("Len: ");
+int l=int.Parse(Console.ReadLine());
+char[] vs = "qwertyuiopasdfghjklzxcvbnm1234567890_.".ToCharArray();
+for(int i=0; i<s; i++)
+{
+    string user = "";
+    for (int j = 0; j < l; j++)
+        user += vs[r.Next(0, 35)];
+    Console.WriteLine(user);
+}
