61616663c25b096a9a0545639fb0b15e3456bde4
ALONE <68062657+haj4@users.noreply.github.com>
2022-03-01 20:35:53 +0200

    Update ListMakerV2.cs

--- ListMakerV2.cs
+++ ListMakerV2.cs
@@ -8,6 +8,6 @@ for(int i=0; i<s; i++)
 {
     string user = "";
     for (int j = 0; j < l; j++)
-        user += vs[r.Next(0, 35)];
+        user += vs[r.Next(0, 37)];
     Console.WriteLine(user);
 }
