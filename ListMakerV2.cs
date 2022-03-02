e92a04fa14be1518100a7f475449401696e9c8c5
ALONE <68062657+haj4@users.noreply.github.com>
2022-03-02 09:26:58 +0200

    Update ListMakerV2.cs

--- ListMakerV2.cs
+++ ListMakerV2.cs
@@ -1,13 +1,17 @@
+using System.IO;
+using System;
 Random r=new Random();
 Console.Write("How much users: ");
 int s = int.Parse(Console.ReadLine());
 Console.Write("Len: ");
 int l=int.Parse(Console.ReadLine());
 char[] vs = "qwertyuiopasdfghjklzxcvbnm1234567890_.".ToCharArray();
-for(int i=0; i<s; i++)
+string newLine = Environment.NewLine;
+for (int i=0; i<s; i++)
 {
     string user = "";
     for (int j = 0; j < l; j++)
         user += vs[r.Next(0, 37)];
-    Console.WriteLine(user);
+    user+= Environment.NewLine;
+    File.AppendAllText(@"your path",user);
 }
